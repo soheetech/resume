8ac2fdaf2a3244535d1595872f8cd49908f7665f
Sohee Moon <soheetech@gmail.com>
2023-10-02 23:03:18 +0900

    상단으로 이동 버튼 추가

--- assets/js/shortcut.js
+++ assets/js/shortcut.js
@@ -1,10 +1,17 @@
 const sections = document.getElementsByTagName('section');
 const navLi = document.getElementsByClassName('shortcut');
+const topBtn = document.getElementById('topbtn');
 
 window.addEventListener('scroll', navHighlighter);
 
 function navHighlighter() {
   let now = document.querySelector('html').scrollTop;
+
+  if (now <= 300) {
+    topBtn.classList.add('hide');
+  } else {
+    topBtn.classList.remove('hide');
+  }
 
   for (i = 0; i < sections.length; i = i + 1) {
     if (now > sections[i].offsetTop - 60 && now < sections[i].offsetTop + sections[i].offsetHeight - 60) {
